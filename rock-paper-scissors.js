806a19d4e631676faefee0cc9d12809133cfd29a
Castle <codelikecastle@gmail.com>
2022-10-31 10:30:27 -0500

    Create function to evaluate selections

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -19,3 +19,40 @@ function getComputerChoice() {
     }
     return computerChoice
 }
+
+function evaluateChoices(playerSelection, computerSelection) {
+    let result
+
+    if (playerSelection == "Rock" && computerSelection == "Rock") {
+        result = "Tie"
+    }
+    else if (playerSelection == "Rock" && computerSelection == "Paper") {
+        result = "Lose"
+    }
+    else if (playerSelection == "Rock" && computerSelection == "Scissor") {
+        result = "Win"
+    }
+    else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        result = "Win"
+    }
+    else if (playerSelection == "Paper" && computerSelection == "Paper") {
+        result = "Tie"
+    }
+    else if (playerSelection == "Paper" && computerSelection == "Scissor") {
+        result = "Lose"
+    }
+    else if (playerSelection == "Scissor" && computerSelection == "Rock") {
+        result = "Lose"
+    }
+    else if (playerSelection == "Scissor" && computerSelection == "Paper") {
+        result = "Win"
+    }
+    else if (playerSelection == "Scissor" && computerSelection == "Scissor") {
+        result = "Tie"
+    }
+    else {
+        result = "Invalid"
+    }
+
+    return result
+}
